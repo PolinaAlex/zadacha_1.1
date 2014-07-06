4c74fc740a2ea04bf27febe32655bc25229dca51
PolinaAlex <polina.volchek.2014@mail.ru>
2014-07-06 21:54:51 +0600

    .

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Нахождение полного сопротивления соединения, который состоит из трех проводников соединенных последовательно.");
+            Console.WriteLine("Введите R1:");
+            Double r1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите R2:");
+            Double r2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите R3:");
+            Double r3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Полное сопротивление соединения={0}", r1 + r2 + r3);
+
+            Console.ReadKey();
+        }
+    }
+}
